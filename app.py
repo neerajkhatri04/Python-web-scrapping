73e0fa8c78ecf8315457ed4d3af9524e82ad2a61
neerajkhatri04 <neerajkha304@gmail.com>
2024-05-06 19:00:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+import logging
+logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import pymongo
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def homepage():
+    return render_template("index.html")
+
+@app.route("/review" , methods = ['POST' , 'GET'])
+def index():
+    if request.method == 'POST':
+        try:
+            searchString = request.form['content'].replace(" ","")
+            print(searchString)
+            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
+            print(flipkart_url)
+            uClient = uReq(flipkart_url)
+            flipkartPage = uClient.read()
+            uClient.close()
+            flipkart_html = bs(flipkartPage, "html.parser")
+            bigboxes = flipkart_html.findAll("div", {"class": "cPHDOP col-12-12"})
+            del bigboxes[0:3]
+            box = bigboxes[0]
+            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            prodRes = requests.get(productLink)
+            prodRes.encoding='utf-8'
+            prod_html = bs(prodRes.text, "html.parser")
+            commentboxes = prod_html.find_all('div', {'class': "RcXBOT"})
+
+            filename = searchString + ".csv"
+            fw = open(filename, "w")
+            headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            fw.write(headers)
+            reviews = []
+            for commentbox in commentboxes:
+                try:
+                    #name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find('p', {'class': '_2NsDsF AwS1CA'}).text
+
+                except:
+                    logging.info("name")
+
+                try:
+                    #rating.encode(encoding='utf-8')
+                    rating = commentbox.div.div.div.div.text
+
+
+                except:
+                    rating = 'No Rating'
+                    logging.info("rating")
+
+                try:
+                    #commentHead.encode(encoding='utf-8')
+                    commentHead = commentbox.div.div.div.p.text
+
+                except:
+                    commentHead = 'No Comment Heading'
+                    logging.info(commentHead)
+                try:
+                    # comtag = commentbox.div.div.find_all('div', {'class': ''})
+                    #custComment.encode(encoding='utf-8')
+                    custComment = commentbox.div.div.find("div", {"class": "ZmyHeo"}).div.div.text
+                except Exception as e:
+                    logging.info(e)
+
+                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                          "Comment": custComment}
+                reviews.append(mydict)
+            logging.info("log my final result {}".format(reviews))
+
+            client = pymongo.MongoClient("mongodb+srv://neerajkha304:rsoBmURCx401xu9X@cluster0.tmefki1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+            db = client["review-scrap"]
+            db_coll = db["review-scrap-data"]
+            db_coll.insert_many(reviews)
+
+            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+        except Exception as e:
+            logging.info(e)
+            return 'something is wrong'
+    # return render_template('results.html')
+
+    else:
+        return render_template('index.html')
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
